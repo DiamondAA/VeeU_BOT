53bdb755704ef54b3d44f809c33e8de705fa7b8f
007Diamond <46367222+DiamondAA@users.noreply.github.com>
2019-03-12 16:45:43 +0700

    Add files via upload

---
+++ veeu_bot.py
@@ -0,0 +1,66 @@
+import requests,time,random,json
+
+head = {
+    "Host": "www.veeuapp.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.10.0"
+}
+
+header = {
+    "Content-Type":"application/json; charset=utf-8",
+    "Content-Length": "140",
+    "Host": "www.veeuapp.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.10.0"
+}
+headDATA2 = {
+    "Host": "www.veeuapp.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.10.0"
+}
+
+headlog = {   
+    "Content-Encoding": "gzip",
+    "Content-Type":"application/json; charset=utf-8",
+    #"Content-Length": 940,
+    "Host": "www.veeuapp.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.10.0"
+    }
+
+video = {
+    'locale': 'in_TH',
+    'task_extra_info': '',
+    'task_name': 'vip_watch_video_ball_01',
+    'timezone': 'GMT+07:00'
+}
+
+def bot(token):
+    medata = requests.post('https://www.veeuapp.com/v1.0/me?access_token='+ token ,headers=head)
+    medata2 = requests.get('https://www.veeuapp.com/v1.0/game/power12?access_token=' + token + '&timezone=GMT%2B07%3A00&locale=th_TH',headers=headDATA2).text
+    jsondata = json.loads(medata.text)
+    if 'user' in jsondata:
+        data = jsondata['user']
+        print('UserID: ' + data['user_id'])
+        print('Name: ' + data['nickname'])
+        print('Email: ' + data['email'])
+        print('Points: ' + str(json.loads(medata2)['total_points']))
+        print('VIP_level: ' + str(json.loads(medata2)['vip_level']))
+        while True:
+            requests.post('https://www.veeuapp.com:443/v1.0/incentive/tasks?access_token=' + token ,data=json.dumps(video),headers=header)
+            medata3 = requests.get('https://www.veeuapp.com/v1.0/game/power12?access_token=' + token + '&timezone=GMT%2B07%3A00&locale=th_TH',headers=headDATA2).text
+            print(time.strftime('%m/%d/%Y - %H:%M:%S  ') + 'Points: ' + str(json.loads(medata3)['total_points']) + '  VIP_level: ' + str(json.loads(medata2)['vip_level']))
+            for i in range(random.randint(4,10)):
+                time.sleep(random.randint(2,7))
+                requests.post('https://www.veeuapp.com:443/v1.0/incentive/tasks?access_token=' + token ,headers=headlog)
+    else:
+        print('ERRER: ' + jsondata['message'])
+
+if __name__ == "__main__":
+    token = input('Token: ')
+    #token = ''
+    bot(token)
